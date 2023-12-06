3087f281caee3a684ab769dc6e76d6f3357ffe8a
Seyran23 <m.seyran23@gmail.com>
2023-12-06 21:01:41 +0400

    finishing Models page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./pages/About";
 import TestimonialsPage from "./pages/TestimonialsPage";
 import Team from "./pages/Team";
 import Contact from "./pages/Contact";
+import Models from "./pages/Models";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route index path="/" element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="models" element={<Models />} />
         <Route path="testimonials" element={<TestimonialsPage />} />
         <Route path="team" element={<Team />} />
         <Route path="contact" element={<Contact />} />
