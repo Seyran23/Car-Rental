dd9fcbf4cd61ba6508737350d023d266bd80f3db
Seyran23 <m.seyran23@gmail.com>
2023-12-06 16:14:37 +0400

    Finishing About, Testimonials, Contact, Team pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.scss';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import TestimonialsPage from "./pages/TestimonialsPage";
+import Team from "./pages/Team";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-      <Home />
-    </div>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="testimonials" element={<TestimonialsPage />} />
+        <Route path="team" element={<Team />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
